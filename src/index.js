aaabb0d0a556a8824f9c60e211743c8d69522b35
zarrok <anakinvv@gmail.com>
2023-03-10 15:25:39 -0500

    formulario_redis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require("express");
 const axios = require("axios");
 const { createClient } = require("redis");
+const { Entity, Schema } = require('redis')
 
 const client = createClient({
   host: "127.0.0.1",
@@ -8,14 +10,36 @@ const client = createClient({
 });
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 (async () => {
   await client.connect();
 })();
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'index.html'));
+});
+
+app.post("/guardar-formulario", (req, res) => {
+  const {nombre,email,telefono,fecha_nacimiento} = req.body;
+
+  const insert = [{
+    "name": nombre,
+    "phone":telefono,
+    "fechaNacimiento":fecha_nacimiento
+  }]
+
+  client.set(email,JSON.stringify(insert));
+
+  res.send("Holi")
+  console.log("Holi")
+});
+
 app.get("/character", async (req, res) => {
   client.get("/character", async (err, reply) => {
     if (reply) {
+      console.log("holi")
       console.log(reply);
     }
   });
